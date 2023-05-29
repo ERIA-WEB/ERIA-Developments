ad6d8a525e2e94fcd3a24ec837b0183869e4d88e
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-05-29 17:35:58 +0700

    Update card_2 news

--- application/views/front-end/common/card_2.php
+++ application/views/front-end/common/card_2.php
@@ -1,6 +1,16 @@
 <?php
 $c2 = $this->header->getPage_card_news();
-$c2['image_name']
+
+
+if (!empty($c2['blk'])) {
+    $news_data['blk'] = $c2['blk'];
+    $title = 'Related News';
+} else {
+    $news_data = $this->header->getLatestNewsPageCard();
+    $title = 'Latest News';
+}
+// id article cards not randoms in DB => 7388,7387,7386
+$c2['image_name'];
 ?>
 <?php
 if (file_exists(FCPATH . $c2['image_name']) && $c2['image_name'] != '') {
@@ -17,11 +27,12 @@ if (file_exists(FCPATH . $c2['image_name']) && $c2['image_name'] != '') {
 </style>
 
 <div class="latest-news-card bg-light-blue px-3 py-4 mb-4">
-    <h4 class="font-merriweather font-weight-bold text-blue mb-3">Latest News</h4>
-    <?php foreach ($c2['blk'] as $key => $nd) {  ?>
+    <h4 class="font-merriweather font-weight-bold text-blue mb-3"><?= $title; ?></h4>
+    <?php foreach ($news_data['blk'] as $key => $nd) {  ?>
     <div class="latest-news-card">
         <p class="mb-1"><small>News</small></p>
-        <a href="<?= base_url() ?>news-and-views/<?= $nd['uri'] ?>" class="font-merriweather font-weight-normal text-blue">
+        <a href="<?= base_url() ?>news-and-views/<?= $nd['uri'] ?>"
+            class="font-merriweather font-weight-normal text-blue">
             <?= $nd['title'] ?>
         </a>
         <p><small><?= $nd['posted_date'] ?></small></p>
