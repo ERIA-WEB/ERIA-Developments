4cae4fcda230c8bca4a7a3538ef023ed7bda866d
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-14 14:19:51 +0700

    Update message detail

--- application/views/front-end/content/message_detail.php
+++ application/views/front-end/content/message_detail.php
@@ -1,17 +1,17 @@
 <link rel="stylesheet" href="<?php echo base_url() ?>v6/css/about-update.css">
 <link rel="stylesheet" href="<?php echo base_url() ?>v6/css/history-update.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/dabase-update.css">
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/database-update.css">
 <style>
-    .message-board-page .image-container {
-        width: 100%;
-        height: auto;
-    }
+.message-board-page .image-container {
+    width: 100%;
+    height: auto;
+}
 
-    @media (min-width: 768px) and (max-width: 991.98px) {
-        .author-detail .title.font-weight-bold {
-            font-size: 0.9rem !important;
-        }
+@media (min-width: 768px) and (max-width: 991.98px) {
+    .author-detail .title.font-weight-bold {
+        font-size: 0.9rem !important;
     }
+}
 </style>
 <div class="container experts-detail-page message-board-page section-top">
     <div class="row mt-3">
@@ -29,7 +29,8 @@
                     </div>
                 </div>
                 <div class="col-md-8 col-12">
-                    <div class="experts-page-title mb-3" style="border-bottom:none;font-size: 18px;"><?php echo $m->title; ?></div>
+                    <div class="experts-page-title mb-3" style="border-bottom:none;font-size: 18px;">
+                        <?php echo $m->title; ?></div>
                     <?php echo $m->content; ?>
                 </div>
             </div>
