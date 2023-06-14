cc02f67800256234ccc13cc9416ecfe4c55350ca
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-14 14:18:23 +0700

    Update contact

--- application/views/front-end/content/contact.php
+++ application/views/front-end/content/contact.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="<?php echo base_url() ?>v6/css/about-update.css">
 <link rel="stylesheet" href="<?php echo base_url() ?>v6/css/history-update.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/dabase-update.css">
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/database-update.css">
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <style>
 @media (max-width: 767.98px) {
