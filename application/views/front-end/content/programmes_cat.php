cb4a9764059b211febf81d2ad611b0b9085fa219
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-16 10:19:53 +0700

    Update programmes category

--- application/views/front-end/content/programmes_cat.php
+++ application/views/front-end/content/programmes_cat.php
@@ -54,84 +54,10 @@ iframe {
     </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script>
-$(document).ready(function() {
-    var start = 0;
-    var limit = 4;
 
-    var reachedMax = false;
+<input type="hidden" id="base_url_front" class="base_url_front" value="<?= base_url(); ?>">
+<script src="<?= base_url(); ?>v6/js/programmes/programmes-categories.js"></script>
 
-    getPost_searchData();
-
-    $('#_msearch').click(function() {
-        start = 0;
-        limit = 5;
-
-        $('.rhead').html("Search Result");
-
-        $('#searchResult').html('');
-
-        getPost_searchData();
-    });
-
-    $('#ldmr').click(function() {
-        getPost_searchData();
-    });
-
-    function getPost_searchData() {
-        var sd = $('#fechasiniestroa').val();
-        var ed = $('#fechasiniestro').val();
-
-        var region = $('#region').val();
-        var url = '<?php echo base_url() ?>Programmes/loadmSearch';
-        var key = $('#key').val();
-
-        $.ajax({
-            url: url,
-            method: 'POST',
-            dataType: 'text',
-            cache: false,
-            data: {
-                getData: 1,
-                start: start,
-                limit: limit,
-                region: region,
-                key: key,
-                sd: sd,
-                ed: ed
-            },
-            success: function(response) {
-                if (response == "") {
-                    $(".loader-image").hide();
-                    $('#ldmr').addClass('d-none');
-                    // $("#ldmr").html("End");
-                    $("#relatedArticles").addClass('d-none');
-                    $(".loadButton").addClass('d-none');
-                    $("#loadButton").addClass('d-none');
-                } else {
-                    $("#ldmr").html("Load More");
-                    $('#ldmr').removeClass('d-none');
-                    $('#normals').show();
-                    $('#normal').hide();
-                    start += limit;
-                    $(".loader-image").show();
-                    $("#searchResult").append(response);
-                    $("#loadButton").removeClass('d-none');
-                    $("#relatedArticles").removeClass('d-none');
-                }
-            }
-        });
-    }
-
-})
-</script>
-<script>
-$('.cnty').click(function() {
-    var type = $(this).data("cnt");
-    $('.region').val(type);
-    $('.reg').html(type);
-})
-</script>
 <?php } else { ?>
 <?php $this->load->view('front-end/content/404/notFound'); ?>
 <?php } ?>
