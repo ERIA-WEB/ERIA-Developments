b29006acdeb2f51188783231611b1580f521b226
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-18 14:47:36 +0700

    Update model

--- application/views/back-end/common/model.php
+++ application/views/back-end/common/model.php
@@ -1,37 +1,21 @@
-<div class="modal" id="section-settings" tabindex="-1" role="dialog" aria-labelledby="ultraModal-Label" aria-hidden="true">
-            <div class="modal-dialog animated bounceInDown">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title">Delete</h4>
-                    </div>
-                    <div class="modal-body n">
+<div class="modal" id="section-settings" tabindex="-1" role="dialog" aria-labelledby="ultraModal-Label"
+    aria-hidden="true">
+    <div class="modal-dialog animated bounceInDown">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Delete</h4>
+            </div>
+            <div class="modal-body n">
 
-                       Do you Really want to delete...?
+                Do you Really want to delete...?
 
-                    </div>
-                    <div class="modal-footer">
-                         <a href="#" class="btn close" data-dismiss="modal">Close</a>
-                        <a href="" class="btn btn-danger delete">Delete</a>
-                    </div>
-                </div>
+            </div>
+            <div class="modal-footer">
+                <a href="#" class="btn close" data-dismiss="modal">Close</a>
+                <a href="" class="btn btn-danger delete">Delete</a>
             </div>
         </div>
-        
-        
-          
-                                        
-                                        
-                                        
-                                        
-                                        
-        <script>
-        
-		$('.close').click(function(){
-			
-			$('.modal').hide();
-			
-			})
-        
-        
-        </script>
+    </div>
+</div>
+<script src="<?= base_url(); ?>v6/js/admin/model.js"></script>
