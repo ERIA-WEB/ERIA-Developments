e4d206b93fb6e431ff891be0dd06e6bac9a12e76
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-19 10:34:16 +0700

    Update create_card

--- application/views/back-end/content/card/create_card.php
+++ application/views/back-end/content/card/create_card.php
@@ -19,15 +19,16 @@
         </div>
         <!--my comment-->
         <div class="clearfix"></div>
-        <form id="login_form" method="POST" enctype="multipart/form-data" accept-charset="utf-8" action="<?php echo base_url(); ?>/system-content/card/insert_card">
+        <form id="login_form" method="POST" enctype="multipart/form-data" accept-charset="utf-8"
+            action="<?php echo base_url(); ?>/system-content/card/insert_card">
             <div class="col-lg-12">
                 <!-- start: Alert Message -->
                 <style>
-                    .help-inline {
-                        color: rgba(240, 80, 80, 1.0);
-                        font-weight: 400;
-                        font-size: 13px;
-                    }
+                .help-inline {
+                    color: rgba(240, 80, 80, 1.0);
+                    font-weight: 400;
+                    font-size: 13px;
+                }
                 </style>
                 <!-- end: Alert Message -->
                 <section class="box">
@@ -49,7 +50,8 @@
                                     <label class="form-label" for="formfield1"> Title Card </label>
                                     <span class="desc">e.g. "Card template 1"</span>
                                     <div class="controls">
-                                        <input type="text" required="required" value="" class="form-control" id="title" name="title" required>
+                                        <input type="text" required="required" value="" class="form-control" id="title"
+                                            name="title" required>
                                     </div>
                                 </div>
                                 <div class="form-group">
@@ -66,15 +68,19 @@
                                 <div id="templateCardFile" class="form-group hidden">
                                     <label class="form-label" for="formfield1"> Upload File Template Cards </label>
                                     <br>
-                                    <span style="font-size: 9px;font-style: italic;color: red;">(Please using extension .php*)</span>
+                                    <span style="font-size: 9px;font-style: italic;color: red;">(Please using extension
+                                        .php*)</span>
                                     <div class="controls">
-                                        <input class="input-file form-control uniform_on focused" id="file_card" value="" name="file_card" type="file" accept=".php/*" placeholder="Upload file card ext .php">
+                                        <input class="input-file form-control uniform_on focused" id="file_card"
+                                            value="" name="file_card" type="file" accept=".php/*"
+                                            placeholder="Upload file card ext .php">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label class="form-label" for="formfield1"> Published </label>
                                     <div style="width: 30px" class="controls">
-                                        <input type="checkbox" value="1" class="form-control" id="published" name="published">
+                                        <input type="checkbox" value="1" class="form-control" id="published"
+                                            name="published">
                                     </div>
                                 </div>
                                 <div class="pull-right">
@@ -91,11 +97,11 @@
             <div class="col-lg-12">
                 <!-- start: Alert Message -->
                 <style>
-                    .help-inline {
-                        color: rgba(240, 80, 80, 1.0);
-                        font-weight: 400;
-                        font-size: 13px;
-                    }
+                .help-inline {
+                    color: rgba(240, 80, 80, 1.0);
+                    font-weight: 400;
+                    font-size: 13px;
+                }
                 </style>
                 <!-- end: Alert Message -->
                 <section id="templateCardCode" class="box hidden">
@@ -111,7 +117,8 @@
                         <div class="row">
                             <div class="col-md-12 col-sm-12 col-xs-12">
                                 <div class="form-group">
-                                    <textarea rows="5" id="summernoteTextArea" class="form-control mytextarea" name="template_content"></textarea>
+                                    <textarea rows="5" id="summernoteTextArea" class="form-control mytextarea"
+                                        name="template_content"></textarea>
                                 </div>
                             </div>
                         </div>
@@ -127,57 +134,31 @@
 <script src="<?php echo base_url() ?>resources/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/js/form-validation.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/plugins/pace/pace.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/perfect-scrollbar/perfect-scrollbar.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/perfect-scrollbar/perfect-scrollbar.min.js"
+    type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/plugins/viewport/viewportchecker.js" type="text/javascript"></script>
 <!-- CORE JS FRAMEWORK - END -->
-<script src="<?php echo base_url() ?>resources/plugins/datatables/js/jquery.dataTables.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/bootstrap/3/dataTables.bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/js/jquery.dataTables.min.js" type="text/javascript">
+</script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js"
+    type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js"
+    type="text/javascript"></script>
+<script
+    src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/bootstrap/3/dataTables.bootstrap.js"
+    type="text/javascript"></script>
 <!-- CORE TEMPLATE JS - START -->
 <script src="<?php echo base_url() ?>resources/js/scripts.js" type="text/javascript"></script>
 <!-- END CORE TEMPLATE JS - END -->
 <!-- Sidebar Graph - START -->
-<script src="<?php echo base_url() ?>resources/plugins/sparkline-chart/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/sparkline-chart/jquery.sparkline.min.js" type="text/javascript">
+</script>
 <script src="<?php echo base_url() ?>resources/js/chart-sparkline.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/js/bootstrap-confirmation.min.js"></script>
 <script src="<?php echo base_url() ?>resources/js/custome.js" type="text/javascript"></script>
-<script>
-    $('#chooseSelectTemplate').on('change', function() {
-        var value = $(this).val();
-        if (value == 'template_file') {
-            $('#templateCardFile').removeClass('hidden');
-            $('#templateCardCode').addClass('hidden');
 
-            $('#file_card').attr("required", "true");
-            // $('#summernoteTextArea').prop('required', false);
-        } else {
-            $('#templateCardCode').removeClass('hidden');
-            $('#templateCardFile').addClass('hidden');
-
-            $('#file_card').prop('required', false);
-            // $('#summernoteTextArea').attr("required", "true");
-
-            // $('.summernote').on('summernote.init', function() {
-            //     $('.summernote').summernote('codeview.activate');
-            // }).summernote({
-            //     height: 1024,
-            //     placeholder: 'Paste content here...',
-            //     codemirror: {
-            //         theme: 'monokai'
-            //     }
-            // });
-        }
-    });
-</script>
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-<script>
-    // $('.summernote').summernote({
-    //     height: 1024,
-    //     placeholder: 'Paste content here...',
-    //     codemirror: {
-    //         theme: 'monokai'
-    //     }
-    // });
-</script>
+
+<input type="hidden" class="base_url_front" value="<?= base_url(); ?>">
+<script src="<?= base_url(); ?>v6/js/admin/cards/create_card.js" type="text/javascript"></script>
