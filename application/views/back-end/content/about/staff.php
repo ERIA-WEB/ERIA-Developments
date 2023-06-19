d1f8fc5501846041be2ecbf8b6d7eb121ab2a6e1
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-19 10:28:05 +0700

    Update staff

--- application/views/back-end/content/about/staff.php
+++ application/views/back-end/content/about/staff.php
@@ -1,7 +1,7 @@
 <style>
-    .dataTables_info {
-        margin-top: 0px !important;
-    }
+.dataTables_info {
+    margin-top: 0px !important;
+}
 </style>
 
 <section id="main-content" class=" ">
@@ -38,20 +38,25 @@
                 <div class="content-body">
                     <div class="row">
                         <div class="col-md-12 col-sm-12 col-xs-12">
-                            <form id="login_form" method="POST" enctype="multipart/form-data" accept-charset="utf-8" action="<?php echo $action; ?>">
+                            <form id="login_form" method="POST" enctype="multipart/form-data" accept-charset="utf-8"
+                                action="<?php echo $action; ?>">
                                 <?php
                                 $csrf = array(
                                     'name' => $this->security->get_csrf_token_name(),
                                     'hash' => $this->security->get_csrf_hash()
                                 );
                                 ?>
-                                <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>" />
-                                <input type="hidden" name="id" value="<?php echo (isset($slider_row)) ? $slider_row->article_id : '' ?>" />
+                                <input type="hidden" name="<?php echo $csrf['name']; ?>"
+                                    value="<?php echo $csrf['hash']; ?>" />
+                                <input type="hidden" name="id"
+                                    value="<?php echo (isset($slider_row)) ? $slider_row->article_id : '' ?>" />
                                 <fieldset>
                                     <div class="masonry-gallery">
                                         <div class="masonry-thumb" style="margin-left: 30%;">
                                             <?php $path = (!isset($slider_row->image_name)) ? "/uploads/keystaffs/slider.jpg" : $slider_row->image_name; ?>
-                                            <img id="placeholder" class="grayscale" src="<?php echo base_url(); ?><?php echo $path; ?>" width="142" alt="Sample Image">
+                                            <img id="placeholder" class="grayscale"
+                                                src="<?php echo base_url(); ?><?php echo $path; ?>" width="142"
+                                                alt="Sample Image">
                                         </div>
                                     </div>
                                 </fieldset>
@@ -63,7 +68,9 @@
                                     <label class="form-label" for="formfield1"> Image </label>
                                     <span class="desc">Dimensions "240 X 340 PX"</span>
                                     <input type="hidden" id="image" name="image" value="" />
-                                    <input class="input-file form-control uniform_on focused" id="photo" value="<?php echo $image; ?>" name="photo" type="file" accept="image/*" placeholder="photo">
+                                    <input class="input-file form-control uniform_on focused" id="photo"
+                                        value="<?php echo $image; ?>" name="photo" type="file" accept="image/*"
+                                        placeholder="photo">
                                     <?php echo form_error('photo', '<span class="help-inline">', '</span>'); ?>
                                 </div>
                         </div>
@@ -76,7 +83,8 @@
                             <span class="desc">e.g. "Annisa Nadhilah Utoro"</span>
                             <div class="controls">
                                 <i class=""></i>
-                                <input type="text" required="required" value="<?php echo $title ?>" class="form-control" id="title" name="title">
+                                <input type="text" required="required" value="<?php echo $title ?>" class="form-control"
+                                    id="title" name="title">
                                 <?php echo form_error('title', '<span class="help-inline">', '</span>'); ?>
                             </div>
                         </div>
@@ -89,7 +97,8 @@
                             <span class="desc">e.g. "Coordinating Officer"</span>
                             <div class="controls">
                                 <i class=""></i>
-                                <input type="text" required="required" value="<?php echo $major ?>" class="form-control" id="major" name="major">
+                                <input type="text" required="required" value="<?php echo $major ?>" class="form-control"
+                                    id="major" name="major">
                                 <?php echo form_error('major', '<span class="help-inline">', '</span>'); ?>
                             </div>
                         </div>
@@ -102,7 +111,8 @@
                             <span class="desc">e.g. "annisanadhilah@eria.com"</span>
                             <div class="controls">
                                 <i class=""></i>
-                                <input type="email" value="<?php echo $majorEmail ?>" class="form-control" id="majorEmail" name="majorEmail">
+                                <input type="email" value="<?php echo $majorEmail ?>" class="form-control"
+                                    id="majorEmail" name="majorEmail">
                                 <?php echo form_error('majorEmail', '<span class="help-inline">', '</span>'); ?>
                             </div>
                         </div>
@@ -115,7 +125,8 @@
                             <span class="desc">e.g. "100"</span>
                             <div class="controls">
                                 <i class=""></i>
-                                <input type="number" required="required" value="<?php echo $order_id ?>" class="form-control" id="order_id" name="order_id">
+                                <input type="number" required="required" value="<?php echo $order_id ?>"
+                                    class="form-control" id="order_id" name="order_id">
                                 <?php echo form_error('order_id', '<span class="help-inline">', '</span>'); ?>
                             </div>
                         </div>
@@ -127,7 +138,8 @@
                             <label class="form-label" for="formfield1"> Published </label>
                             <div style="width: 30px" class="controls">
                                 <i class=""></i>
-                                <input type="checkbox" value="1" <?php if ($published == 1) { ?> checked <?php } ?> class="form-control" id="published" name="published">
+                                <input type="checkbox" value="1" <?php if ($published == 1) { ?> checked <?php } ?>
+                                    class="form-control" id="published" name="published">
                                 <?php echo form_error('published', '<span class="help-inline">', '</span>'); ?>
                             </div>
                         </div>
@@ -154,7 +166,8 @@
                 <div class="content-body" style="background:#f5f5f5;overflow:auto;">
                     <div class="row">
                         <div class="col-md-12 col-sm-12 col-xs-12" id="dvContents">
-                            <table id="examples" style="font-size:12px;" class="display table table-hover table-condensed" cellspacing="0" width="100%">
+                            <table id="examples" style="font-size:12px;"
+                                class="display table table-hover table-condensed" cellspacing="0" width="100%">
                                 <thead>
                                     <tr>
                                         <th>No</th>
@@ -171,16 +184,18 @@
                                     <?php $x = 0;
                                     foreach ($areaList->result() as $id => $area) {
                                         $x++; ?>
-                                        <tr>
-                                            <td><?php echo $x; ?> </td>
-                                            <td><?php echo $area->title; ?></td>
-                                            <td><a href="#" class="pop"> <img src="<?= base_url() ?>get_thumbs.php?im=<?php echo $area->image_name; ?>" width="110"> </a> </td>
-                                            <td><?php echo $area->major; ?></td>
-                                            <td><?php echo $area->video_url; ?></td>
-                                            <td><?php echo $area->order_id; ?></td>
-                                            <td>
-                                                <?php $session_user = $this->session->userdata('logged_in'); ?>
-                                                <?php
+                                    <tr>
+                                        <td><?php echo $x; ?> </td>
+                                        <td><?php echo $area->title; ?></td>
+                                        <td><a href="#" class="pop"> <img
+                                                    src="<?= base_url() ?>get_thumbs.php?im=<?php echo $area->image_name; ?>"
+                                                    width="110"> </a> </td>
+                                        <td><?php echo $area->major; ?></td>
+                                        <td><?php echo $area->video_url; ?></td>
+                                        <td><?php echo $area->order_id; ?></td>
+                                        <td>
+                                            <?php $session_user = $this->session->userdata('logged_in'); ?>
+                                            <?php
                                                 if ($area->published == 0) {
                                                     $btnstatus = 'data-btn-ok-class="btn btn-success" data-status="1" data-btn-ok-label="Published" data-placement="left" class="btn btn-warning  pub-callback"';
                                                 } else {
@@ -192,9 +207,9 @@
                                                 echo $status_action['status'];
                                                 ?>
 
-                                            </td>
-                                            <td class="hidden-print">
-                                                <?php
+                                        </td>
+                                        <td class="hidden-print">
+                                            <?php
                                                 $edit_action = $this->privilage->edit('edit', $session_user['user_id'], 'About/editS/', $area->article_id);
                                                 $delete_action = $this->privilage->delete('delete', $session_user['user_id'], $area->article_id);
                                                 // get action edit
@@ -202,8 +217,8 @@
                                                 // get action delete
                                                 echo $delete_action['delete'];
                                                 ?>
-                                            </td>
-                                        </tr>
+                                        </td>
+                                    </tr>
                                     <?php } ?>
                                 </tbody>
                             </table>
@@ -219,7 +234,8 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-body">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span
+                        class="sr-only">Close</span></button>
                 <img src="" class="imagepreview" style="width: 100%;">
             </div>
         </div>
@@ -231,132 +247,29 @@
 <script src="<?php echo base_url() ?>resources/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/js/form-validation.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/plugins/pace/pace.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/perfect-scrollbar/perfect-scrollbar.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/perfect-scrollbar/perfect-scrollbar.min.js"
+    type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/plugins/viewport/viewportchecker.js" type="text/javascript"></script>
 <!-- CORE JS FRAMEWORK - END -->
-<script src="<?php echo base_url() ?>resources/plugins/datatables/js/jquery.dataTables.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/bootstrap/3/dataTables.bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/js/jquery.dataTables.min.js" type="text/javascript">
+</script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js"
+    type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js"
+    type="text/javascript"></script>
+<script
+    src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/bootstrap/3/dataTables.bootstrap.js"
+    type="text/javascript"></script>
 <!-- CORE TEMPLATE JS - START -->
 <script src="<?php echo base_url() ?>resources/js/scripts.js" type="text/javascript"></script>
 <!-- END CORE TEMPLATE JS - END -->
 
 <!-- Sidebar Graph - START -->
-<script src="<?php echo base_url() ?>resources/plugins/sparkline-chart/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/sparkline-chart/jquery.sparkline.min.js" type="text/javascript">
+</script>
 <script src="<?php echo base_url() ?>resources/js/chart-sparkline.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/js/bootstrap-confirmation.min.js"></script>
 <script src="<?php echo base_url() ?>resources/js/custome.js" type="text/javascript"></script>
-<script>
-    var delete_id = null;
-    var delete_tr = null;
-    var name = null;
 
-    $('.confirmation-callback').click(function() {
-        delete_id = $(this).data("id");
-        name = $(this).data("area");
-        delete_tr = $(this).closest('tr');
-
-        confirmationCallbackDelete(delete_id, name, delete_tr);
-    });
-
-    function confirmationCallbackDelete(delete_id, name, delete_tr) {
-        $('.confirmation-callback').confirmation({
-            singleton: true,
-            onConfirm: function(event, element) {
-                $.ajax({
-                    type: "POST",
-                    url: "<?php echo base_url(); ?>system-content/About/deleter",
-                    data: {
-                        id: delete_id,
-                        name: name
-
-                    }
-                }).done(function(json) {
-                    delete_tr.css("background-color", "#FF0000");
-                    delete_tr.fadeOut(1200, function() {
-                        delete_tr.remove();
-                    });
-                })
-            }
-        });
-    }
-</script>
-<script>
-    $(function() {
-        $('.pop').on('click', function() {
-            $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-            $('#imagemodal').modal('show');
-        });
-    });
-</script>
-<script>
-    $('#photo').change(function() {
-        var input = this;
-        var name = $(this).val();
-
-        $('#image').val(name);
-
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                $('#placeholder').attr('src', e.target.result).attr('width', 142);
-            };
-            reader.readAsDataURL(input.files[0]);
-        }
-    });
-</script>
-
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-
-<script>
-    $(document).ready(function() {
-        $('#summernote').summernote();
-        $('#article_keywords').summernote();
-    });
-</script>
-
-<script>
-    var delete_id = null;
-    var delete_tr = null;
-    var status = null;
-
-    $('.pub-callback').click(function() {
-        delete_id = $(this).data("id");
-        status = $(this).data("status");
-        delete_tr = $(this).closest('tr');
-
-        publishR(delete_id, status, delete_tr);
-    });
-
-    function publishR(delete_id, status, delete_tr) {
-        $('.pub-callback').confirmation({
-            singleton: true,
-            title: "Publish confirmation",
-            onConfirm: function(event, element) {
-                $.ajax({
-                    type: "POST",
-                    url: "<?php echo base_url(); ?>system-content/research/publishR",
-                    data: {
-                        id: delete_id,
-                        pub: status
-
-                    }
-                }).done(function(json) {
-                    delete_tr.css("background-color", "yellow");
-                    delete_tr.fadeOut(1200, function() {
-                        location.reload();
-                    });
-                })
-            }
-        });
-    }
-    $(document).ready(function() {
-        $('#examples').DataTable({
-            order: [
-                [2, 'desc']
-            ],
-        });
-    });
-</script>
+<input type="hidden" class="base_url_front" value="<?= base_url(); ?>">
+<script src="<?= base_url(); ?>v6/js/admin/abouts/staff.js" type="text/javascript"></script>
