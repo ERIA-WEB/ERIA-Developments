9ed112fd5710d5a2597e7d6af2a21a0248f5029b
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-20 10:09:54 +0700

    Update top sub

--- application/views/back-end/content/card/top_sub.php
+++ application/views/back-end/content/card/top_sub.php
@@ -3,7 +3,6 @@
     margin-top: -50px !important;
 }
 </style>
-
 <section id="main-content">
     <section class="wrapper main-wrapper">
         <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
@@ -58,7 +57,6 @@
                                     <label class="form-label" for="formfield1"> Form Title </label>
                                     <span class="desc">e.g. "PROGRAMS"</span>
                                     <div class="controls">
-                                        <i class=""></i>
                                         <input type="text" value="<?= $home_title ?>" class="form-control"
                                             id="home_title" name="home_title">
                                         <?php echo form_error('home_title', '<span class="help-inline">', '</span>'); ?>
@@ -69,9 +67,7 @@
                                     <div class="controls">
                                         <?php
                                             $article_topics = $this->Card_model->getCategoryResearchAreasCardByCategoryType('topics');
-
                                             $results = $this->privilage->cacheManager($article_topics, 'card_article_topics');
-
                                             $categoryIds = explode(', ', $contentData->sub_heading);
                                             $category_research_data = $this->Card_model->getCategoryResearchById($categoryIds);
                                         ?>
@@ -85,14 +81,11 @@
                                                     echo '<option value="">--Select Topics--</option>';
                                                 }
                                             ?>
-
                                             <?php
                                                 foreach ($results as $key => $value) {
                                                     echo '<option value="'.$value->category_id.'">'.$value->category_name.'</option>';
                                                 }
                                             ?>
-
-                                            <option value="2">Test 2</option>
                                         </select>
                                     </div>
                                 </div>
@@ -103,7 +96,6 @@
                                     ?>
                                     <label class="form-label" for="formfield3"> Number of Data </label>
                                     <div class="controls">
-                                        <i class=""></i>
                                         <input type="number" value="<?= $contentData->content ?>" class="form-control"
                                             id="headinng" name="headinng">
                                         <?php echo form_error('headinng', '<span class="help-inline">', '</span>'); ?>
@@ -151,11 +143,6 @@
 <script src="<?php echo base_url() ?>resources/js/chart-sparkline.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/js/bootstrap-confirmation.min.js"></script>
 <script src="<?php echo base_url() ?>resources/js/custome.js" type="text/javascript"></script>
-<script>
-$("#other_topics").select2({
-    placeholder: 'Choose Other Topics',
-    allowClear: true
-}).on('select2-open', function() {
-    $(this).data('select2').results.addClass('overflow-hidden').perfectScrollbar();
-});
-</script>
+
+<input type="hidden" class="base_url_front" value="<?= base_url(); ?>">
+<script src="<?= base_url(); ?>v6/js/admin/cards/top_sub.js" type="text/javascript"></script>
