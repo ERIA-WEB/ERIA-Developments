e29aa4d6260fff7e90dc9a1a0de0720b0b7b4e44
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-20 10:08:32 +0700

    Update so_sub

--- application/views/back-end/content/card/so_sub.php
+++ application/views/back-end/content/card/so_sub.php
@@ -1,9 +1,8 @@
 <style>
-    .dataTables_info {
-        margin-top: -50px !important;
-    }
+.dataTables_info {
+    margin-top: -50px !important;
+}
 </style>
-
 <section id="main-content">
     <section class="wrapper main-wrapper">
         <div class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
@@ -26,7 +25,8 @@
         </div>
         <!--my comment-->
         <div class="clearfix"></div>
-        <form id="login_form" method="POST" enctype="multipart/form-data" accept-charset="utf-8" action="<?php echo $action; ?>">
+        <form id="login_form" method="POST" enctype="multipart/form-data" accept-charset="utf-8"
+            action="<?php echo $action; ?>">
             <div class="col-lg-6"><?php $this->load->view('back-end/common/message'); ?>
                 <section class="box ">
                     <header class="panel_header">
@@ -46,8 +46,10 @@
                                     'hash' => $this->security->get_csrf_hash()
                                 );
                                 ?>
-                                <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>" />
-                                <input type="hidden" name="id" value="<?php echo (isset($contentData)) ? $contentData->c_id : '' ?>" />
+                                <input type="hidden" name="<?php echo $csrf['name']; ?>"
+                                    value="<?php echo $csrf['hash']; ?>" />
+                                <input type="hidden" name="id"
+                                    value="<?php echo (isset($contentData)) ? $contentData->c_id : '' ?>" />
                                 <div class="form-group">
                                     <?php
                                     $error = (form_error('home_title') === '') ? '' : 'error';
@@ -56,7 +58,8 @@
                                     <label class="form-label" for="formfield1"> Share Social Media </label>
                                     <span class="desc">e.g. " "</span>
                                     <div class="controls">
-                                        <input type="text" value="<?= $home_title ?>" class="form-control" id="home_title" name="home_title">
+                                        <input type="text" value="<?= $home_title ?>" class="form-control"
+                                            id="home_title" name="home_title">
                                         <?php echo form_error('home_title', '<span class="help-inline">', '</span>'); ?>
                                     </div>
                                 </div>
@@ -67,7 +70,8 @@
                                     ?>
                                     <label class="form-label" for="formfield3">Print</label>
                                     <div class="controls">
-                                        <input type="text" value="<?= $contentData->sub_heading ?>" class="form-control" id="headinng" name="headinng">
+                                        <input type="text" value="<?= $contentData->sub_heading ?>" class="form-control"
+                                            id="headinng" name="headinng">
                                         <?php echo form_error('headinng', '<span class="help-inline">', '</span>'); ?>
                                     </div>
                                 </div>
@@ -89,18 +93,25 @@
 <script src="<?php echo base_url() ?>resources/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/js/form-validation.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/plugins/pace/pace.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/perfect-scrollbar/perfect-scrollbar.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/perfect-scrollbar/perfect-scrollbar.min.js"
+    type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/plugins/viewport/viewportchecker.js" type="text/javascript"></script>
 <!-- CORE JS FRAMEWORK - END -->
-<script src="<?php echo base_url() ?>resources/plugins/datatables/js/jquery.dataTables.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/bootstrap/3/dataTables.bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/js/jquery.dataTables.min.js" type="text/javascript">
+</script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js"
+    type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js"
+    type="text/javascript"></script>
+<script
+    src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/bootstrap/3/dataTables.bootstrap.js"
+    type="text/javascript"></script>
 <!-- CORE TEMPLATE JS - START -->
 <script src="<?php echo base_url() ?>resources/js/scripts.js" type="text/javascript"></script>
 <!-- END CORE TEMPLATE JS - END -->
 <!-- Sidebar Graph - START -->
-<script src="<?php echo base_url() ?>resources/plugins/sparkline-chart/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/sparkline-chart/jquery.sparkline.min.js" type="text/javascript">
+</script>
 <script src="<?php echo base_url() ?>resources/js/chart-sparkline.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/js/bootstrap-confirmation.min.js"></script>
 <script src="<?php echo base_url() ?>resources/js/custome.js" type="text/javascript"></script>
