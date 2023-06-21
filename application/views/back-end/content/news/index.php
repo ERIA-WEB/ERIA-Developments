adbb4a5929facdbbd911cbd5c5958fede040ff48
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-21 10:04:35 +0700

    Update backend index news

--- application/views/back-end/content/news/index.php
+++ application/views/back-end/content/news/index.php
@@ -179,84 +179,9 @@
 <script src="<?php echo base_url() ?>resources/js/chart-sparkline.js" type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/js/bootstrap-confirmation.min.js"></script>
 <script src="<?php echo base_url() ?>resources/js/custome.js" type="text/javascript"></script>
-<script>
-var delete_id = null;
-var delete_tr = null;
-var name = null;
-
-$('.confirmation-callback').click(function() {
-    delete_id = $(this).data("id");
-    name = $(this).data("area");
-    delete_tr = $(this).closest('tr');
-});
-
-$('.confirmation-callback').confirmation({
-
-    singleton: true,
-
-    onConfirm: function(event, element) {
-
-
-
-        $.ajax({
-            type: "POST",
-            url: "<?php echo base_url(); ?>system-content/user/deleteUser",
-            data: {
-                id: delete_id,
-                name: name
-
-            }
-        }).done(function(json) {
-
-
-            delete_tr.css("background-color", "#FF0000");
-            delete_tr.fadeOut(1200, function() {
-                delete_tr.remove();
-            });
-
-
-        })
-
-
-    }
-});
-</script>
-
-
-<script>
-$(function() {
-    $('.pop').on('click', function() {
-        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-        $('#imagemodal').modal('show');
-    });
-});
-</script>
-
-
-
-
-<script>
-$('#photo').change(function() {
-    var input = this;
-    var name = $(this).val();
-
-    $('#image').val(name);
-
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            $('#placeholder').attr('src', e.target.result).attr('width', 142);
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
-});
-</script>
 
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
-<script>
-$(document).ready(function() {
-    $('#summernote').summernote();
-});
-</script>
+<input type="hidden" class="base_url_front" value="<?= base_url(); ?>">
+<script src="<?= base_url(); ?>v6/js/admin/news/index.js" type="text/javascript"></script>
