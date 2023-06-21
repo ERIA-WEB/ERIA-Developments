6a52a31faa38d6010751a813f0e32447353507c7
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-21 10:00:47 +0700

    Update index events

--- application/views/back-end/content/Events/index.php
+++ application/views/back-end/content/Events/index.php
@@ -1,7 +1,7 @@
 <style>
-    .dataTables_info {
-        margin-top: -50px !important;
-    }
+.dataTables_info {
+    margin-top: -50px !important;
+}
 </style>
 
 <section id="main-content" class=" ">
@@ -62,7 +62,8 @@
 
 
 
-                            <form id="login_form" method="POST" enctype="multipart/form-data" accept-charset="utf-8" action="<?php echo $action; ?>">
+                            <form id="login_form" method="POST" enctype="multipart/form-data" accept-charset="utf-8"
+                                action="<?php echo $action; ?>">
 
 
                                 <?php
@@ -71,8 +72,10 @@
                                     'hash' => $this->security->get_csrf_hash()
                                 );
                                 ?>
-                                <input type="hidden" name="<?php echo $csrf['name']; ?>" value="<?php echo $csrf['hash']; ?>" />
-                                <input type="hidden" name="id" value="<?php echo (isset($slider_row)) ? $slider_row->page_id : '' ?>" />
+                                <input type="hidden" name="<?php echo $csrf['name']; ?>"
+                                    value="<?php echo $csrf['hash']; ?>" />
+                                <input type="hidden" name="id"
+                                    value="<?php echo (isset($slider_row)) ? $slider_row->page_id : '' ?>" />
 
 
 
@@ -97,7 +100,8 @@
                                     <span class="desc">e.g. "Events"</span>
                                     <div class="controls">
                                         <i class=""></i>
-                                        <input type="text" required="required" value="<?php echo $menu_title ?>" class="form-control" id="menu_title" name="menu_title">
+                                        <input type="text" required="required" value="<?php echo $menu_title ?>"
+                                            class="form-control" id="menu_title" name="menu_title">
                                         <?php echo form_error('menu_title', '<span class="help-inline">', '</span>'); ?>
 
                                     </div>
@@ -119,7 +123,8 @@
                                     <span class="desc">e.g. "Innovation Policy in/for ASEAN' Study 2nd Workshop"</span>
                                     <div class="controls">
                                         <i class=""></i>
-                                        <input type="text" required="required" value="<?php echo $title ?>" class="form-control" id="title" name="title">
+                                        <input type="text" required="required" value="<?php echo $title ?>"
+                                            class="form-control" id="title" name="title">
                                         <?php echo form_error('title', '<span class="help-inline">', '</span>'); ?>
 
                                     </div>
@@ -162,7 +167,8 @@
 
 
 
-                                        <textarea rows="5" style="height: 250px" id="summernote" class="form-control mytextarea" name="content"><?= $content ?></textarea>
+                                        <textarea rows="5" style="height: 250px" id="summernote"
+                                            class="form-control mytextarea" name="content"><?= $content ?></textarea>
 
 
 
@@ -190,7 +196,8 @@
                                     <span class="desc">e.g. "100"</span>
                                     <div class="controls">
                                         <i class=""></i>
-                                        <input type="number" required="required" value="<?php echo $order_id ?>" class="form-control" id="order_id" name="order_id">
+                                        <input type="number" required="required" value="<?php echo $order_id ?>"
+                                            class="form-control" id="order_id" name="order_id">
                                         <?php echo form_error('order_id', '<span class="help-inline">', '</span>'); ?>
 
                                     </div>
@@ -213,7 +220,8 @@
                                     <span class="desc">e.g. "100"</span>
                                     <div style="width: 30px" class="controls">
                                         <i class=""></i>
-                                        <input type="checkbox" value="1" <?php if ($published == 1) { ?> checked <?php } ?> class="form-control" id="published" name="published">
+                                        <input type="checkbox" value="1" <?php if ($published == 1) { ?> checked
+                                            <?php } ?> class="form-control" id="published" name="published">
                                         <?php echo form_error('published', '<span class="help-inline">', '</span>'); ?>
 
                                     </div>
@@ -240,7 +248,8 @@
 
 
 
-                                        <input type="text" value="<?= $meta_keywords ?>" class="form-control" id="meta_keywords" name="meta_keywords">
+                                        <input type="text" value="<?= $meta_keywords ?>" class="form-control"
+                                            id="meta_keywords" name="meta_keywords">
 
 
                                         <?php echo form_error('meta_keywords', '<span class="help-inline">', '</span>'); ?>
@@ -267,7 +276,8 @@
 
 
 
-                                        <input type="text" value="<?= $meta_description ?>" class="form-control" id="meta_description" name="meta_description">
+                                        <input type="text" value="<?= $meta_description ?>" class="form-control"
+                                            id="meta_description" name="meta_description">
 
 
                                         <?php echo form_error('meta_description', '<span class="help-inline">', '</span>'); ?>
@@ -321,7 +331,8 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-body">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span
+                        class="sr-only">Close</span></button>
                 <img src="" class="imagepreview" style="width: 100%;">
             </div>
         </div>
@@ -345,7 +356,8 @@
 <script src="<?php echo base_url() ?>resources/js/form-validation.js" type="text/javascript"></script>
 
 <script src="<?php echo base_url() ?>resources/plugins/pace/pace.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/perfect-scrollbar/perfect-scrollbar.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/perfect-scrollbar/perfect-scrollbar.min.js"
+    type="text/javascript"></script>
 <script src="<?php echo base_url() ?>resources/plugins/viewport/viewportchecker.js" type="text/javascript"></script>
 
 
@@ -353,10 +365,15 @@
 
 
 
-<script src="<?php echo base_url() ?>resources/plugins/datatables/js/jquery.dataTables.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js" type="text/javascript"></script>
-<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/bootstrap/3/dataTables.bootstrap.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/js/jquery.dataTables.min.js" type="text/javascript">
+</script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/TableTools/js/dataTables.tableTools.min.js"
+    type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/js/dataTables.responsive.min.js"
+    type="text/javascript"></script>
+<script
+    src="<?php echo base_url() ?>resources/plugins/datatables/extensions/Responsive/bootstrap/3/dataTables.bootstrap.js"
+    type="text/javascript"></script>
 
 
 <!-- CORE TEMPLATE JS - START -->
@@ -364,94 +381,12 @@
 <!-- END CORE TEMPLATE JS - END -->
 
 <!-- Sidebar Graph - START -->
-<script src="<?php echo base_url() ?>resources/plugins/sparkline-chart/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url() ?>resources/plugins/sparkline-chart/jquery.sparkline.min.js" type="text/javascript">
+</script>
 <script src="<?php echo base_url() ?>resources/js/chart-sparkline.js" type="text/javascript"></script>
 
-
 <script src="<?php echo base_url() ?>resources/js/bootstrap-confirmation.min.js"></script>
-
-
 <script src="<?php echo base_url() ?>resources/js/custome.js" type="text/javascript"></script>
 
-
-<script>
-    var delete_id = null;
-    var delete_tr = null;
-    var name = null;
-
-    $('.confirmation-callback').click(function() {
-        delete_id = $(this).data("id");
-        name = $(this).data("area");
-        delete_tr = $(this).closest('tr');
-    });
-
-    $('.confirmation-callback').confirmation({
-
-        singleton: true,
-
-        onConfirm: function(event, element) {
-
-
-
-            $.ajax({
-                type: "POST",
-                url: "<?php echo base_url(); ?>system-content/user/deleteUser",
-                data: {
-                    id: delete_id,
-                    name: name
-
-                }
-            }).done(function(json) {
-
-
-                delete_tr.css("background-color", "#FF0000");
-                delete_tr.fadeOut(1200, function() {
-                    delete_tr.remove();
-                });
-
-
-            })
-
-
-        }
-    });
-</script>
-
-
-<script>
-    $(function() {
-        $('.pop').on('click', function() {
-            $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-            $('#imagemodal').modal('show');
-        });
-    });
-</script>
-
-
-
-
-<script>
-    $('#photo').change(function() {
-        var input = this;
-        var name = $(this).val();
-
-        $('#image').val(name);
-
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-            reader.onload = function(e) {
-                $('#placeholder').attr('src', e.target.result).attr('width', 142);
-            };
-            reader.readAsDataURL(input.files[0]);
-        }
-    });
-</script>
-
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
-
-<script>
-    $(document).ready(function() {
-        // $('#summernote').summernote();
-    });
-</script>
+<input type="hidden" class="base_url_front" value="<?= base_url(); ?>">
+<script src="<?= base_url(); ?>v6/js/admin/events/index.js" type="text/javascript"></script>
