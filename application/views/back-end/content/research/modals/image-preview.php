d8fbc2b64f126bdae64d761787d607eeba8088f3
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-22 09:51:03 +0700

    Update backend

--- application/views/back-end/content/research/modals/image-preview.php
+++ application/views/back-end/content/research/modals/image-preview.php
@@ -2,7 +2,9 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-body">
-                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <button type="button" class="close" data-dismiss="modal">
+                    <span aria-hidden="true">&times;</span><span class="sr-only">Close</span>
+                </button>
                 <img src="" class="imagepreview" style="width: 100%;">
             </div>
         </div>
