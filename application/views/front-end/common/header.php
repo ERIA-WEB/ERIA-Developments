4cc74523b255f115e8e33d64ddd10819fe0e047a
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-25 14:49:21 +0700

    update header

--- application/views/front-end/common/header.php
+++ application/views/front-end/common/header.php
@@ -2,6 +2,7 @@
     <title><?= $title; ?></title>
     <meta http-equiv="Content-type" value="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Permissions-Policy" content="interest-cohort=()">
     <?php $this->load->view('front-end/common/script-meta'); ?>
     <?php $this->load->view('front-end/common/script-css'); ?>
 </head>
