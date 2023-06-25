b82cd33b053ce23e682b8b6f97ac726a16720a96
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-25 14:50:08 +0700

    update template

--- application/views/front-end/common/template.php
+++ application/views/front-end/common/template.php
@@ -16,53 +16,7 @@
     </div>
     <?php $this->load->view($content); ?>
     <?php $this->load->view('front-end/common/footer'); ?>
-    <script>
-    // Create cookie
-    function setCookie(cname, cvalue, exdays) {
-        const d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        let expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
-
-    // Read cookie
-    function getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
-
-    function acceptCookieConsent() {
-        setCookie('user_cookie_consent', 1, 30);
-        const cookieNoticeElement = document.getElementById("cookieNotice");
-        const classes = cookieNoticeElement.classList;
-        classes.remove("d-flex");
-        document.getElementById("cookieNotice").className += " d-none";
-    }
-
-    $(document).ready(function() {
-        let cookie_consent = getCookie("user_cookie_consent");
-        if (cookie_consent != "") {
-            const cookieNoticeElement = document.getElementById("cookieNotice");
-            const classes = cookieNoticeElement.classList;
-            classes.remove("d-flex");
-            document.getElementById("cookieNotice").className += " d-none";
-        } else {
-            document.getElementById("cookieNotice").className += " d-flex";
-            document.getElementById("cookieNotice").classList.remove("d-none");
-        }
-    });
-    </script>
+    <script type="text/javascript" src="<?= base_url(); ?>v6/js/cookies.min.js" async></script>
 </body>
 
 </html>
