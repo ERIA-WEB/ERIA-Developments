135b5053fb95494acc4c271d680825b02a7c3ae9
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-06-25 14:49:46 +0700

    update scriptcss

--- application/views/front-end/common/script-css.php
+++ application/views/front-end/common/script-css.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>v6/css/normalize.css" />
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/variables.css">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>v6/css/normalize.min.css" media="all" />
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/variables.min.css" media="all">
 <link rel="stylesheet" href="<?php echo base_url() ?>v6/css/theme.min.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/modify.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/styles.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/header.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/custome_wyiswg.css">
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/modify.min.css" media="all">
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/styles.min.css" media="all">
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/header.min.css" media="all">
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/custome_wyiswg.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 <link rel="stylesheet" href="<?php echo base_url() ?>v6/css/swiper-bundle.min.css" />
 <script src="<?php echo base_url() ?>v6/assets/js/swiper-bundle.min.js"></script>
