6b5ca2965f10272c3d48f0946e5e9b018d2b9096
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-07-06 10:11:49 +0700

    Update script css

--- application/views/front-end/common/script-css.php
+++ application/views/front-end/common/script-css.php
@@ -1,12 +1,13 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/font-awesome.min.css" rel="preload" as="style">
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>v6/css/normalize.min.css" media="all" />
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/variables.min.css" media="all">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/theme.min.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/modify.min.css" media="all">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/styles.min.css" media="all">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/header.min.css" media="all">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/custome_wyiswg.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/swiper-bundle.min.css" />
-<script src="<?php echo base_url() ?>v6/assets/js/swiper-bundle.min.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>v6/css/normalize.min.css" rel="preload"
+    as="style" />
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/variables.min.css" rel="preload" as="style" />
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/theme.min.css" rel="preload" as="style" />
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/modify.min.css" rel="preload" as="style" />
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/styles.min.css" rel="preload" as="style" />
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/header.min.css" rel="preload" as="style" />
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/custome_wyiswg.min.css" rel="preload" as="style" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" />
+<link rel="stylesheet" href="<?php echo base_url() ?>v6/css/swiper-bundle.min.css" rel="preload" as="style" />
+<script src="<?php echo base_url() ?>v6/assets/js/swiper-bundle.min.js" async></script>
