d87b011ae8bba2530189693d742e8177d9b89899
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-07-07 10:10:39 +0700

    Update login_new

--- application/views/back-end/content/login_new.php
+++ application/views/back-end/content/login_new.php
@@ -4,48 +4,59 @@ $bgs = glob("resources/login-bgs/*.jpg");
 
 
 // $this->sma->print_arrays($bgs);
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title><?= $title ?></title>
-    <script type="text/javascript">if (parent.frames.length !== 0) { top.location = '<?=base_url()?>'; }</script>
+    <script type="text/javascript">
+    if (parent.frames.length !== 0) {
+        top.location = '<?=base_url()?>';
+    }
+    </script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="shortcut icon" href="<?php echo base_url() ?>resources/images/favicon.png" type="image/x-icon" /> 
-    <link href="<?= base_url() ?>resources/login/theme.css" rel="stylesheet"/>
-    <link href="<?= base_url() ?>resources/login/style.css" rel="stylesheet"/>
-    <link href="<?= base_url() ?>resources/login/helpers/login.css" rel="stylesheet"/>
+    <link rel="shortcut icon" href="<?php echo base_url() ?>resources/images/favicon.png" type="image/x-icon" />
+    <link href="<?= base_url() ?>resources/login/theme.css" rel="stylesheet" />
+    <link href="<?= base_url() ?>resources/login/style.css" rel="stylesheet" />
+    <link href="<?= base_url() ?>resources/login/helpers/login.css" rel="stylesheet" />
     <script type="text/javascript" src="<?= base_url() ?>resources/login/js/jquery-2.0.3.min.js"></script>
     <!--[if lt IE 9]>
     <script src="<?= $assets ?>js/respond.min.js"></script>
     <![endif]-->
     <style>
-        body {
-            min-width: 350px;
-        }
-        .bblue {
-            background: #fff !important;
-        }
-        .login-page .page-back {
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-            justify-content: center;
-            background-size: cover !important;
-            background-position: center !important;
-            background-image: url("<?=base_url().$bgs[mt_rand(0, count($bgs) - 1)] ?>") !important;
-        }
-        .contents {
-            margin: 16px;
-            border-radius: 6px;
-            padding: 32px 16px;
-            background: rgba(0, 0, 0, 0.1);
-            border: 1px solid rgba(0, 0, 0, 0.2);
-        }
-        .login-content, .login-page .login-form-links {
-            margin-top: 20px;
-            border-radius: 6px;
-        }
+    body {
+        min-width: 350px;
+    }
+
+    .bblue {
+        background: #fff !important;
+    }
+
+    .login-page .page-back {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+        background-size: cover !important;
+        background-position: center !important;
+        background-image: url("<?=base_url().$bgs[mt_rand(0, count($bgs) - 1)] ?>") !important;
+    }
+
+    .contents {
+        margin: 16px;
+        border-radius: 6px;
+        padding: 32px 16px;
+        background: rgba(0, 0, 0, 0.1);
+        border: 1px solid rgba(0, 0, 0, 0.2);
+    }
+
+    .login-content,
+    .login-page .login-form-links {
+        margin-top: 20px;
+        border-radius: 6px;
+    }
     </style>
 
 </head>
@@ -55,7 +66,8 @@ $bgs = glob("resources/login-bgs/*.jpg");
         <div class="global-site-notice noscript">
             <div class="notice-inner">
                 <p>
-                    <strong>JavaScript seems to be disabled in your browser.</strong><br>You must have JavaScript enabled in
+                    <strong>JavaScript seems to be disabled in your browser.</strong><br>You must have JavaScript
+                    enabled in
                     your browser to utilize the functionality of this website.
                 </p>
             </div>
@@ -64,7 +76,7 @@ $bgs = glob("resources/login-bgs/*.jpg");
     <div class="page-back">
         <div class="contents">
             <div class="text-center">
-     <img src="<?= base_url() ?>v6/assets/logo.png" width="200" alt="" style="margin-bottom:10px;   " />
+                <img src="<?= base_url() ?>v6/assets/logo.webp" width="200" alt="" style="margin-bottom:10px;   " />
             </div>
 
             <div id="login">
@@ -72,62 +84,64 @@ $bgs = glob("resources/login-bgs/*.jpg");
 
                     <div class="login-form-div">
                         <div class="login-content">
-<?php
+                            <?php
                              if ($this->session->flashdata('error-message') != '') { ?>
-                              
-                                <div class="alert alert-danger">
-                                    <button data-dismiss="alert" class="close" type="button">×</button>
-                                    <ul class="list-group"><?php echo $this->session->flashdata('error-message'); ?></ul>
-                                </div>
-                                <?php
+
+                            <div class="alert alert-danger">
+                                <button data-dismiss="alert" class="close" type="button">×</button>
+                                <ul class="list-group"><?php echo $this->session->flashdata('error-message'); ?></ul>
+                            </div>
+                            <?php
                             }
                             if ($this->session->flashdata('success-message') != '') {
                                 ?>
-                                <div class="alert alert-success">
-                                    <button data-dismiss="alert" class="close" type="button">×</button>
-                                    <ul class="list-group"><?php echo $this->session->flashdata('success-message'); ?></ul>
-                                </div>
-                                <?php
+                            <div class="alert alert-success">
+                                <button data-dismiss="alert" class="close" type="button">×</button>
+                                <ul class="list-group"><?php echo $this->session->flashdata('success-message'); ?></ul>
+                            </div>
+                            <?php
                             }
                             ?>
-                            
-                            
+
+
                             <?php echo form_open('system-content/Login/validate', 'class=" " id=" " '); ?>
-                            
+
                             <div class="div-title col-sm-12">
-                                <h3 class="text-primary">    Log in to your Account </h3>
+                                <h3 class="text-primary"> Log in to your Account </h3>
                             </div>
                             <div class="col-sm-12">
                                 <div class="textbox-wrap form-group">
                                     <div class="input-group">
                                         <span class="input-group-addon"><i class="fa fa-user"></i></span>
-                                        <input type="text" value="" required="required" class="form-control" name="username"
-                                        placeholder=""/>
+                                        <input type="text" value="" required="required" class="form-control"
+                                            name="username" placeholder="" />
                                     </div>
                                 </div>
                                 <div class="textbox-wrap form-group">
                                     <div class="input-group">
                                         <span class="input-group-addon"><i class="fa fa-key"></i></span>
-                                        <input type="password" value="" required="required" class="form-control " name="password"
-                                        placeholder=""/>
+                                        <input type="password" value="" required="required" class="form-control "
+                                            name="password" placeholder="" />
                                     </div>
                                 </div>
                             </div>
-                            
+
                             <div class="form-action col-sm-12">
                                 <div class="checkbox pull-left">
                                     <div class="custom-checkbox">
                                         <?php echo form_checkbox('remember', '1', false, 'id="remember"'); ?>
                                     </div>
-                                    <span class="checkbox-text pull-left"><label for="remember">Remember Me</label></span>
+                                    <span class="checkbox-text pull-left"><label for="remember">Remember
+                                            Me</label></span>
                                 </div>
-                                <button type="submit" class="btn btn-success pull-right">Login &nbsp; <i class="fa fa-sign-in"></i></button>
+                                <button type="submit" class="btn btn-success pull-right">Login &nbsp; <i
+                                        class="fa fa-sign-in"></i></button>
                             </div>
                             <?php echo form_close(); ?>
                             <div class="clearfix"></div>
                         </div>
                         <div class="login-form-links link2">
-                            <h4 class="text- ">Forgot your password ?  </h4>
+                            <h4 class="text- ">Forgot your password ? </h4>
 
                             <a href="#forgot_password" class="text-danger forgot_password_link">Click here to reset </a>
 
@@ -154,7 +168,7 @@ $bgs = glob("resources/login-bgs/*.jpg");
                         <div class="login-content">
 
                             <div class="div-title col-sm-12">
-                                <h3 class="text-primary">  Reset Password </h3>
+                                <h3 class="text-primary"> Reset Password </h3>
                             </div>
 
                             <?php echo form_open('system-content/Login/forgot_password', 'class="login" data-toggle="validator"'); ?>
@@ -166,7 +180,7 @@ $bgs = glob("resources/login-bgs/*.jpg");
                                     <div class="input-group">
                                         <span class="input-group-addon "><i class="fa fa-envelope"></i></span>
                                         <input type="email" name="forgot_email" class="form-control "
-                                               placeholder="Email Address" required="required"/>
+                                            placeholder="Email Address" required="required" />
                                     </div>
                                 </div>
                                 <div class="form-action">
@@ -188,22 +202,23 @@ $bgs = glob("resources/login-bgs/*.jpg");
 
 
         </div>
-             
-        </div>
+
+    </div>
     </div>
     <script src="<?= base_url() ?>resources/login/js/jquery.js"></script>
     <script src="<?= base_url() ?>resources/login/js/bootstrap.min.js"></script>
     <script src="<?= base_url() ?>resources/login/js/jquery.cookie.js"></script>
     <script src="<?= base_url() ?>resources/login/js/login.js"></script>
     <script type="text/javascript">
-        $(document).ready(function () {
-            localStorage.clear();
-            var hash = window.location.hash;
-            if (hash && hash != '') {
-                $("#login").hide();
-                $(hash).show();
-            }
-        });
+    $(document).ready(function() {
+        localStorage.clear();
+        var hash = window.location.hash;
+        if (hash && hash != '') {
+            $("#login").hide();
+            $(hash).show();
+        }
+    });
     </script>
 </body>
+
 </html>
