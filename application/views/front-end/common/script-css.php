cffa57ad93127f4e1cb3d0979755c5d7d8eb11c4
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-07-08 15:30:33 +0700

    Update File Script CSS

--- application/views/front-end/common/script-css.php
+++ application/views/front-end/common/script-css.php
@@ -10,4 +10,4 @@
 <link rel="stylesheet" href="<?php echo base_url() ?>v6/css/custome_wyiswg.min.css" rel="preload" as="style" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" />
 <link rel="stylesheet" href="<?php echo base_url() ?>v6/css/swiper-bundle.min.css" rel="preload" as="style" />
-<script src="<?php echo base_url() ?>v6/assets/js/swiper-bundle.min.js" async></script>
+<script src="<?php echo base_url() ?>v6/assets/js/swiper-bundle.min.js"></script>
