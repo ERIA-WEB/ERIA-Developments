d56b622a6482543d7058678a0f097ea6cc9cad24
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-07-10 18:50:36 +0700

    Create JS

---
+++ v6/js/admin/cards/recent.js
@@ -0,0 +1,42 @@
+
+var delete_id = null;
+var delete_tr = null;
+var name = null;
+
+$('.confirmation-callback').click(function () {
+    delete_id = $(this).data("id");
+    var name = $(this).data("area");
+    delete_tr = $(this).closest('tr');
+});
+
+$('.confirmation-callback').confirmation({
+
+    singleton: true,
+
+    onConfirm: function (event, element) {
+
+
+
+        $.ajax({
+            type: "POST",
+            url: base_url_front + "system-content/Mmenu/deleteR",
+            data: {
+                id: delete_id,
+                name: name
+
+            }
+        }).done(function (json) {
+
+
+            delete_tr.css("background-color", "#FF0000");
+            delete_tr.fadeOut(1200, function () {
+                delete_tr.remove();
+                location.reload();
+            });
+
+
+        })
+
+
+    }
+});
