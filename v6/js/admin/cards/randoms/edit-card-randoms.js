47db2a3e5b6b717a3cd11f0aceb8fc21110d8a56
Gumilar L Wijayadi <gumilarlesmana@gmail.com>
2023-07-10 18:49:41 +0700

    Create JS

---
+++ v6/js/admin/cards/randoms/edit-card-randoms.js
@@ -0,0 +1,17 @@
+
+$('#chooseSelectTemplate').on('change', function () {
+    var value = $(this).val();
+    if (value == 'template_file') {
+        $('#templateCardFile').removeClass('hidden');
+        $('#templateCardCode').addClass('hidden');
+
+        $('#file_card').attr("required", "true");
+        $('#summernoteTextArea').prop('required', false);
+    } else {
+        $('#templateCardCode').removeClass('hidden');
+        $('#templateCardFile').addClass('hidden');
+
+        $('#file_card').prop('required', false);
+        $('#summernoteTextArea').attr("required", "true");
+    }
+});
